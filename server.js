4ea1fd51ec33a10dab96b83eb7ff49536ec40201
nadiTime <nadavcn@gmail.com>
2016-10-29 17:21:18 +0200

    init mean project

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+
+var app = express();
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+
+
+// Initialize the app.
+var server = app.listen(process.env.PORT || 8080, function () {
+	var port = server.address().port;
+	console.log("App now running on port", port);
+});
